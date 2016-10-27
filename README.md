19ebc6e3b7474d0474cb6a8663d8f72fd9f1cd97
MarcLazo <lazolatorre@et.esiea.fr>
2016-10-27 16:33:15 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+AdoptADoc
+===========
+
+A Symfony project created on October 27, 2016, 12:36 pm.
+Is a social network for physician
---
+++ src/AppBundle/Controller/SiteController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class SiteController extends Controller
+{
+    public function indexAction(){
+
+        return $this->render('AppBundle:Default:index.html.twig');
+    }
+
+    public function CreateAccountAction (){
+
+        return $this->render('AppBundle:Default:index.html.twig');
+    }
+
+    public function showAccountAction (){
+
+        return $this->render('AppBundle:Default:index.html.twig');
+    }
+
+    public function ViewAddAction (){
+
+        return $this->render('AppBundle:Default:index.html.twig');
+    }
+
+    public function EditAddAction (){
+
+        return $this->render('AppBundle:Default:index.html.twig');
+    }
+
+    public function NewAddAction (){
+
+        return $this->render('AppBundle:Default:index.html.twig');
+    }
+
+    public function DeleteAddAction (){
+
+        return $this->render('AppBundle:Default:index.html.twig');
+    }
+
+}
