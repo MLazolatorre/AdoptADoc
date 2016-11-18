8887071661d4318bc5c7095f98bfbe285a76b9ad
MarcLazo <lazolatorre@et.esiea.fr>
2016-11-18 18:46:46 +0100

    Controler homePage et viewAdvert OK

--- src/AppBundle/Controller/SiteController.php
+++ src/AppBundle/Controller/SiteController.php
@@ -4,20 +4,23 @@ namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use AppBundle\Repository\AdvertRepository;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class SiteController extends Controller{
 
     public function indexAction(){
 
-        $emAdvert = $this->getDoctrine()->getRepository('AppBundle:Advert');
-        $listAdvert = $emAdvert->findAll();
+        //we select all the advert
+        $advertRepository = $this->getDoctrine()->getRepository('AppBundle:Advert');
+        $listAdvert = $advertRepository->findAll();
 
+        //TODO renomer la page (default:index....)
         return $this->render('AppBundle:Default:index.html.twig', array(
             'listAdvert' => $listAdvert,
         ));
     }
 
-    public function CreateAccountAction (){
+    public function createAccountAction (){
 
         return $this->render('AppBundle:Default:index.html.twig');
     }
@@ -27,9 +30,21 @@ class SiteController extends Controller{
         return $this->render('AppBundle:Default:index.html.twig');
     }
 
-    public function ViewAddAction (){
+    public function viewAddAction ($id)
+    {
+        //we select the advert id
+        $advertRepository = $this->getDoctrine()->getRepository('AppBundle:Advert');
+        $advert = $advertRepository->find($id);
 
-        return $this->render('AppBundle:Default:index.html.twig');
+        //we check if the advert exist
+        if ($advert == null){
+            throw new NotFoundHttpException('The page with the id : ' . $id . "dosen't exist");
+        }
+
+        //TODO renomer la page (default:index....)
+        return $this->render('AppBundle:Default:index.html.twig', array(
+            'advert'    => $advert,
+        ));
     }
 
     public function EditAddAction (){
