cd56a127c780532b2d050c0fd076cc2158351daa
MarcLazo <lazolatorre@et.esiea.fr>
2016-11-22 14:12:22 +0100

    On peut stocker les users mais pas encore se loger avec

--- src/AppBundle/Controller/SiteController.php
+++ src/AppBundle/Controller/SiteController.php
@@ -7,43 +7,61 @@ use AppBundle\Entity\Application;
 use AppBundle\Form\AdvertType;
 use AppBundle\Form\ApplicationType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\Debug\Exception\UndefinedFunctionException;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use UserBundle\Entity\User;
+use UserBundle\Form\UserType;
 
 
-class SiteController extends Controller{
+class SiteController extends Controller
+{
 
-    public function indexAction(){
+    public function indexAction()
+    {
 
         //we select all the advert
         $advertRepository = $this->getDoctrine()->getRepository('AppBundle:Advert');
         $listAdvert = $advertRepository->findAll();
 
-        return $this->render('AppBundle:Site:menu.html.twig ', array(
+        return $this->render('AppBundle:Site:menu.html.twig', array(
             'listAdvert' => $listAdvert,
         ));
     }
 
-    public function createAccountAction ()
+    public function createAccountAction(Request $request)
+    {
+        $user = new User();
+
+        $form = $this->get('form.factory')->create(UserType::class, $user);
+
+        if ($request->isMethod('POST') && $form->handleRequest($request)->isValid()) {
+            //we check in JS if password == confirm password
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            return $this->redirectToRoute('app_homepage');
+        }
+
+        return $this->render('@User/Security/create_account.html.twig', array(
+            'form' => $form->createView(),
+
+        ));
+    }
+
+    public function showAccountAction($id)
     {
         return $this->render('AppBundle:Default:index.html.twig');
     }
 
-    public function showAccountAction ()
-    {
-        return $this->render('AppBundle:Default:index.html.twig');
-    }
-
-    public function viewAddAction (Request $request, $id)
+    public function viewAdAction(Request $request, $id)
     {
         //we look for the advert in the DB with the ID '$id'
         $advertRepository = $this->getDoctrine()->getRepository('AppBundle:Advert');
         $advert = $advertRepository->find($id);
 
         //we check if the advert exist
-        if ($advert == null){
+        if ($advert == null) {
             throw new NotFoundHttpException("The page with the id : " . $id . " dosen't exist");
         }
 
@@ -52,89 +70,95 @@ class SiteController extends Controller{
         $form = $this->get('form.factory')->create(ApplicationType::class, $application);
         $em = $this->getDoctrine()->getManager();
 
-        if ($request->isMethod('POST') && $form->handleRequest($request)->isValid())
-        {
+        if ($request->isMethod('POST') && $form->handleRequest($request)->isValid()) {
             $em->persist($application);
             $em->flush();
         }
 
+        //we find all the application
         $listApplication = $em->getRepository('AppBundle:Application')->findAll();
 
-        return $this->render('AppBundle:Sit:viewAdd.html.twig', array(
-            'advert'    => $advert,
-            'form'      => $form->createView(),
-            'listApplication'   => $listApplication,
+        $advertApplication = array();
+
+        foreach ($listApplication as $application) {
+            if ($application->getAdvert() == $advert) {
+                $advertApplication[] = $application;
+            }
+        }
+
+        return $this->render('AppBundle:Sit:viewAd.html.twig', array(
+            'advert'          => $advert,
+            'form'            => $form->createView(),
+            'listApplication' => $advertApplication,
         ));
     }
 
-    public function newAddAction (Request $request)
+    public function newAdAction(Request $request)
     {
         $advert = new Advert();
 
         $form = $this->get('form.factory')->create(AdvertType::class, $advert);
 
         //if we receive a POST request => the use has sent the form so we sent his new page
-        if ($request->isMethod('POST') && $form->handleRequest($request)->isValid()){
+        if ($request->isMethod('POST') && $form->handleRequest($request)->isValid()) {
 
             $em = $this->getDoctrine()->getManager();
             $em->persist($advert);
             $em->flush();
 
-            return $this->redirectToRoute('app_viewAdd', array(
+            return $this->redirectToRoute('app_viewAd', array(
                 'id' => $advert->getId(),
             ));
         }
 
         return $this->render('AppBundle:Sit:newAdvert.html.twig', array(
-            'form'  =>  $form->createView(),
+            'form' => $form->createView(),
         ));
     }
 
-    public function editAddAction ($id, Request $request)
+    public function editAdAction($id, Request $request)
     {
         //we look for the advert in the DB with the ID '$id'
         $em = $this->getDoctrine()->getManager();
         $advert = $em->getRepository('AppBundle:Advert')->find($id);
 
         //we check if the id exist
-        if ($advert == null){
+        if ($advert == null) {
             throw new NotFoundHttpException ("The page with the id : " . $id . " dosen't exist");
         }
 
         $form = $this->get('form.factory')->create(AdvertType::class, $advert);
 
         //if POST => the use try to edit the advert
-        if ($request->isMethod('POST') && $form->handleRequest($request)->isValid())
-        {
+        if ($request->isMethod('POST') && $form->handleRequest($request)->isValid()) {
             //no persist because the advert exited already
             $em->flush();
 
-            return $this->redirectToRoute('app_viewAdd', array(
-                'id'    => $id,
+            return $this->redirectToRoute('app_viewAd', array(
+                'id' => $id,
             ));
         }
 
         return $this->render('@App/Sit/newAdvert.html.twig', array(       //TODO KIDJO : il faut rediriger la page
-            'form'  => $form->createView(),
+            'form' => $form->createView(),
         ));
     }
 
-    public function deleteAddAction (Request $request ,$id)
+    public function deleteAdAction(Request $request, $id)
     {
         //we look for the advert in the DB with the ID '$id'
         $em = $this->getDoctrine()->getManager();
         $advert = $em->getRepository('AppBundle:Advert')->find($id);
 
         //check if the advert exist
-        if ($advert == null ){
+        if ($advert == null) {
             throw new NotFoundHttpException("The page with the id : " . $id . " dosen't exist");
         }
 
         $form = $this->get('form.factory')->create();
 
         //check if we receive a post query => the user want to delete the advert
-        if ($request->isMethod('POST') && $form->handleRequest($request)->isValid())
-        {
+        if ($request->isMethod('POST') && $form->handleRequest($request)->isValid()) {
             //we remove the advert
             $em->remove($advert);
             $em->flush();
@@ -145,8 +169,8 @@ class SiteController extends Controller{
 
         //if there is no POST method we return the delete page
         return $this->render('@App/Sit/delete.html.twig', array(
-            'advert'    =>  $advert,
-            'form'      =>  $form->createView(),
+            'advert' => $advert,
+            'form'   => $form->createView(),
         ));
     }
 
---
+++ src/UserBundle/Entity/User.php
@@ -0,0 +1,149 @@
+<?php
+
+namespace UserBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * User
+ *
+ * @ORM\Table(name="user")
+ * @ORM\Entity(repositoryClass="UserBundle\Repository\UserRepository")
+ */
+class User
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="username", type="string", length=255, unique=true)
+     */
+    private $username;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="password", type="string", length=255)
+     */
+    private $password;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="mail", type="string", length=255)
+     */
+    private $mail;
+
+    /**
+     * @var string
+     */
+    private $confirmPassword;
+
+
+    /**
+     * @return string
+     */
+    public function getConfirmPassword()
+    {
+        return $this->confirmPassword;
+    }
+
+    /**
+     * @param string $confirmPassword
+     */
+    public function setConfirmPassword($confirmPassword)
+    {
+        $this->confirmPassword = $confirmPassword;
+    }
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set username
+     *
+     * @param string $username
+     *
+     * @return User
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    /**
+     * Get username
+     *
+     * @return string
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * Set password
+     *
+     * @param string $password
+     *
+     * @return User
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Get password
+     *
+     * @return string
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * Set mail
+     *
+     * @param string $mail
+     *
+     * @return User
+     */
+    public function setMail($mail)
+    {
+        $this->mail = $mail;
+
+        return $this;
+    }
+
+    /**
+     * Get mail
+     *
+     * @return string
+     */
+    public function getMail()
+    {
+        return $this->mail;
+    }
+}
+
